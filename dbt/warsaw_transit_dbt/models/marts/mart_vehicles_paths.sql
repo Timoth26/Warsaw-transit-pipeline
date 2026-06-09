e388e58b09a30ead6fe0e430cdc26d682c6d908f
Tomasz <tomekmich26@gmail.com>
2026-06-09 14:39:19 +0200

    Fix vehicles paths - add duration

--- dbt/warsaw_transit_dbt/models/marts/mart_vehicles_paths.sql
+++ dbt/warsaw_transit_dbt/models/marts/mart_vehicles_paths.sql
@@ -1,11 +1,13 @@
 {{ config(materialized='table') }}
 
 SELECT
-    vehicle_id,
+    vehicle_number,
     vehicle_type,
     -- Dla każdego pojazdu zbieramy współrzędne w chronologiczną tablicę [lon, lat]
     -- Uwaga: W dbt-athena/Presto czasami współrzędne dla deck.gl buduje się jako JSON lub tablicę obiektów
-    array_agg(array[lon, lat] ORDER BY event_time) AS path_coordinates
+    array_agg(array[lon, lat] ORDER BY event_time) AS path_coordinates,
+    MIN(event_time) AS route_start_time,
+    MAX(event_time) AS route_end_time
 
 FROM {{ ref('stg_transit') }}
 WHERE lat IS NOT NULL AND lon IS NOT NULL
